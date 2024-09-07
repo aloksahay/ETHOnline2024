b54463f60519ac09b98a3a006e27233275fa3e1f
rere <regis-graptin@sfr.fr>
2024-09-07 23:57:52 +0200

    feat: add token bound account creation through the SDK

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,15 @@
 require('dotenv').config()
 
+const { spicy } = require('viem/chains');
+
 const ethers = require('ethers');
 const express = require("express");
 
+const { TokenboundClient } = require('@tokenbound/sdk');
+
 const PORT = process.env.PORT || 3000;
+
+const SPICY_TESTNET_CHAIN_ID = 88882;
 
 const app = express();
 
@@ -35,9 +41,12 @@ app.get("/create/:user_address", async (request, response) => {
     try {
         let user_address = request.params['user_address'];
 
-        let provider = new ethers.JsonRpcProvider("https://spicy-rpc.chiliz.com/");
+        let provider = new ethers.JsonRpcProvider("https://spicy-rpc.chiliz.com/", );
 
         let signer = new ethers.Wallet(SERVER_KEY, provider);
+
+        // Token bound SDK
+        const tokenboundClient = new TokenboundClient({ signer, chain: spicy });
 
         let contract = new ethers.Contract(
             NFT_CONTRACT_ADDRESS,
@@ -46,10 +55,22 @@ app.get("/create/:user_address", async (request, response) => {
         );
 
         let res = await contract.buyTShirt(user_address);
-        await res.wait();
-        
+
+        // Retrieve the nft id
+        let response_data = await res.wait();
+        let log = response_data.logs.filter((log) => log.fragment.name == "NewTShirtBuy");
+        let token_id = log[0].args[0]        
+
+        // Create token bound account
+        const { account, txHash } = await tokenboundClient.createAccount({
+            tokenContract: NFT_CONTRACT_ADDRESS,
+            tokenId: token_id,
+        })
+
         response.send({
             "hash": res["hash"],
+            "tokenId": token_id.toString(),
+            "account": account,
         });
 
     } catch (error) {
